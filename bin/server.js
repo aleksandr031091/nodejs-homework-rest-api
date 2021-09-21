764b7927471ff48d75ad0a6a6c1bb4faaee500db
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-09-21 13:29:33 +0300

    start hw03 add db,check server metods

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,48 @@
-const app = require('../app')
+const app = require("../app");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000
+const { DB_HOST, PORT = 3000 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+// const { Schema, model } = mongoose;
+
+// const contactSchema = Schema({
+//   name: String,
+//   email: String,
+//   phone: String,
+// });
+
+// const Contact = model("contact", contactSchema);
+// const newContact = {
+//   name: "sasha",
+//   email: "sasha@gmail.com",
+//   phone: "12345",
+// };
+
+mongoose
+  .connect(DB_HOST, {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+    // Contact.create(newContact, (error, data) => {
+    //   console.log(error);
+    //   console.log(data);
+    // });
+    // Contact.find({}, (error, data) => {
+    //   console.log(error);
+    //   console.log(data);
+    // });
+    // console.log("db is connect");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`);
+// });
