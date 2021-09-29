46b2fc7e451733b8f14a281a0143f76928493338
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-09-29 13:55:58 +0300

    initial commit

---
+++ model/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const UserShema = Schema({
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter",
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+const joiSchema = Joi.object({
+  password: Joi.string().min(1).required(),
+  email: Joi.string().email().required(),
+  subscription: Joi.string(),
+  token: Joi.string(),
+});
+
+const User = model("user", UserShema);
+
+module.exports = {
+  User,
+  joiSchema,
+};
