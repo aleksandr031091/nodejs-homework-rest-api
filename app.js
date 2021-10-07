f209c9576ea94455044935f75247e57c8502def5
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-10-07 15:17:06 +0300

    update avatar

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/api/auth");
+const usersRouter = require("./routes/api/users");
 
 const app = express();
 
@@ -13,15 +14,18 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/v1/contacts", contactsRouter);
-app.use("/api/v1/users", authRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", usersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, _, res, __) => {
+  console.log(err);
   res.status(500).json({ message: err.message });
 });
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {
-  controllerWraper,
-  validation,
+const { controllerWraper, authenticate, upload } = require("../../middlewares");
+const { users: ctrl } = require("../../controllers");
+
+router.patch(
+  "/avatars",
   authenticate,
-} = require("../../middlewares");
-const { joiSchema } = require("../../model/user");
-const { auth: ctrl } = require("../../controllers");
+  upload.single("avatar"),
+  controllerWraper(ctrl.updateAvatar)
+);
 
 module.exports = router;
